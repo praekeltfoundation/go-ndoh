92730701644f61ee5dbc162625327cabd7e1ccbc
gsvr <gsvanrooyen@gmail.com>
2014-04-16 09:42:45 +0200

    remove non-applicable code and add clinic_code state

--- src/clinic.js
+++ src/clinic.js
@@ -3,7 +3,7 @@ go.clinic = function() {
     var App = vumigo.App;
     var Choice = vumigo.states.Choice;
     var ChoiceState = vumigo.states.ChoiceState;
-    var EndState = vumigo.states.EndState;
+    // var EndState = vumigo.states.EndState;
     var FreeText = vumigo.states.FreeText;
 
     var GoNDOHclinic = App.extend(function(self) {
@@ -18,7 +18,7 @@ go.clinic = function() {
                             'registered?'),
 
                 choices: [
-                    new Choice('states:suspect_pregnancy', 'Yes'),
+                    new Choice('states:clinic_code', 'Yes'),
                     new Choice('states:mom_number', 'No'),
                 ],
 
@@ -28,150 +28,21 @@ go.clinic = function() {
             });
         });
 
-        self.states.add('states:suspect_pregnancy', function(name) {
-            return new ChoiceState(name, {
-                question: $('MomConnect sends free support SMSs to ' +
-                    'pregnant mothers. Are you or do you suspect that you ' +
-                    'are pregnant?'),
-
-                choices: [
-                    new Choice('states:id_type', $('Yes')),
-                    new Choice('states:end_not_pregnant', $('No')),
-                ],
-
-                next: function(choice) {
-                    return choice.value;
-                }
-            });
-        });
-
-        self.states.add('states:end_not_pregnant', function(name) {
-            return new EndState(name, {
-                text: $('We are sorry but this service is only for ' +
-                    'pregnant mothers. If you have other health concerns ' +
-                    'please visit your nearest clinic.'),
-                next: 'states:start'
-            });
-        });
-
-        // text shortened - too many characters
-        self.states.add('states:id_type', function(name) {
-            return new ChoiceState(name, {
-                question: $('We need some info to message you. This ' +
-                    'is private and will only be used to help you at a ' +
-                    'clinic. What kind of ID do you have?'),
-
-                choices: [
-                    new Choice('states:sa_id', $('SA ID')),
-                    new Choice('states:passport_origin', $('Passport')),
-                    new Choice('states:birth_year', $('None')),
-                ],
-
-                next: function(choice) {
-                    return choice.value;
-                }
-            });
-        });
-
-        self.states.add('states:sa_id', function(name) {
+        self.states.add('states:clinic_code', function(name) {
             return new FreeText(name, {
-                question: $('Please enter your SA ID number:'),
+                question: $('Please input the mobile number of the ' +
+                            'pregnant woman to be registered:'),
 
                 next: function() {
-                    return 'states:end_success';
+                    return 'states:id_type';
                 }
             });
         });
 
-        self.states.add('states:passport_origin', function(name) {
-            return new ChoiceState(name, {
-                question: $('What is the country of origin of the passport?'),
+        
 
-                choices: [
-                    new Choice('states:passport_no', $('Zimbabwe')),
-                    new Choice('states:passport_no', $('Mozambique')),
-                    new Choice('states:passport_no', $('Malawi')),
-                    new Choice('states:passport_no', $('Nigeria')),
-                    new Choice('states:passport_no', $('DRC')),
-                    new Choice('states:passport_no', $('Somalia')),
-                    new Choice('states:passport_no', $('Other')),
-                ],
 
-                next: function(choice) {
-                    return choice.value;
-                }
-            });
-        });
-
-        self.states.add('states:passport_no', function(name) {
-            return new FreeText(name, {
-                question: $('Please enter your Passport number:'),
-
-                next: function() {
-                    return 'states:end_success';
-                }
-            });
-        });
-
-        self.states.add('states:birth_year', function(name) {
-            return new FreeText(name, {
-                question: $('Since you don\'t have an ID or passport, ' +
-                    'please enter the year that you were born (eg ' +
-                    '1981)'),
-
-                next: function() {
-                    return 'states:birth_month';
-                }
-            });
-        });
-
-        self.states.add('states:birth_month', function(name) {
-            return new ChoiceState(name, {
-                question: $('Please enter the month that you were born.'),
-
-                choices: [
-                    new Choice('states:birth_day', $('Jan')),
-                    new Choice('states:birth_day', $('Feb')),
-                    new Choice('states:birth_day', $('March')),
-                    new Choice('states:birth_day', $('April')),
-                    new Choice('states:birth_day', $('May')),
-                    new Choice('states:birth_day', $('June')),
-                    new Choice('states:birth_day', $('July')),
-                    new Choice('states:birth_day', $('August')),
-                    new Choice('states:birth_day', $('Sept')),
-                    new Choice('states:birth_day', $('Oct')),
-                    new Choice('states:birth_day', $('Nov')),
-                    new Choice('states:birth_day', $('Dec')),
-                ],
-
-                next: function(choice) {
-                    return choice.value;
-                }
-            });
-        });
-
-        self.states.add('states:birth_day', function(name) {
-            return new FreeText(name, {
-                question: $('Please enter the day that you were born ' +
-                    '(eg 14).'),
-
-                next: function() {
-                    return 'states:end_success';
-                }
-            });
-        });
-
-        // text shortened - too many characters
-        self.states.add('states:end_success', function(name) {
-            return new EndState(name, {
-                text: $('Thank you for subscribing to MomConnect. ' +
-                    'You will now start receiving free messages ' +
-                    'about MomConnect. Remember to visit your ' +
-                    'nearest clinic!'),
-                next: 'states:start'
-            });
-        });
-
+        
     });
 
     return {
--- test/clinic.test.js
+++ test/clinic.test.js
@@ -41,214 +41,25 @@ describe("app", function() {
             });
         });
 
-        describe("when the user selects a language", function() {
-            it("should ask them if they suspect pregnancy", function() {
+        describe("when the no. is the pregnant woman's no.", function() {
+            it("should ask for the clinic code", function() {
                 return tester
                     .setup.user.state('states:start')
-                    .input('1') /* change language state functionality */
-                    .check.interaction({
-                        state: 'states:suspect_pregnancy',
-                        reply: [
-                            'MomConnect sends free support SMSs to ' +
-                            'pregnant mothers. Are you or do you suspect ' +
-                            'that you are pregnant?',
-                            '1. Yes',
-                            '2. No'
-                        ].join('\n')
-                    })
-                    .run();
-            });
-        });
-
-        describe("if the user does not suspect pregnancy", function() {
-            it("state service is for pregnant mothers and exit", function() {
-                return tester
-                    .setup.user.state('states:suspect_pregnancy')
-                    .input('2')
-                    .check.interaction({
-                        state: 'states:end_not_pregnant',
-                        reply: ('We are sorry but this service is only for ' +
-                            'pregnant mothers. If you have other health ' +
-                            'concerns please visit your nearest clinic.')
-                    })
-                    .check.reply.ends_session()
-                    .run();
-            });
-        });
-
-        describe("if the user suspects pregnancy", function() {
-            it("should ask for their id type", function() {
-                return tester
-                    .setup.user.state('states:suspect_pregnancy')
                     .input('1')
                     .check.interaction({
-                        state: 'states:id_type',
-                        reply: [
-                            'We need some info to message you. This is ' +
-                            'private and will only be used to help you at ' +
-                            'a clinic. What kind of ID do you have?',
-                            '1. SA ID',
-                            '2. Passport',
-                            '3. None'
-                        ].join('\n')
+                        state: 'states:clinic_code',
+                        reply: (
+                            'Please input the mobile number of the ' +
+                            'pregnant woman to be registered:')
                     })
                     .run();
             });
         });
 
-        describe("if the user selects SA ID (id type)", function() {
-            it("should ask for their id number", function() {
-                return tester
-                    .setup.user.state('states:id_type')
-                    .input('1')
-                    .check.interaction({
-                        state: 'states:sa_id',
-                        reply: 'Please enter your SA ID number:'
-                    })
-                    .run();
-            });
-        });
+        
 
-        describe("after the user enters their ID number", function() {
-            it("should thank them and exit", function() {
-                return tester
-                    .setup.user.state('states:sa_id')
-                    .input('7001011234050')
-                    .check.interaction({
-                        state: 'states:end_success',
-                        reply: ('Thank you for subscribing to MomConnect. ' +
-                            'You will now start receiving free messages ' +
-                            'about MomConnect. Remember to visit your ' + 
-                            'nearest clinic!')
-                    })
-                    .check.reply.ends_session()
-                    .run();
-            });
-        });
 
-        describe("if the user selects Passport (id type)", function() {
-            it("should ask for their country of origin", function() {
-                return tester
-                    .setup.user.state('states:id_type')
-                    .input('2')
-                    .check.interaction({
-                        state: 'states:passport_origin',
-                        reply: ['What is the country of origin of the ' +
-                            'passport?',
-                            '1. Zimbabwe',
-                            '2. Mozambique',
-                            '3. Malawi',
-                            '4. Nigeria',
-                            '5. DRC',
-                            '6. Somalia',
-                            '7. Other'
-                        ].join('\n')
-                    })
-                    .run();
-            });
-        });
-
-        describe("after the user selects passport country", function() {
-            it("should ask for their passport number", function() {
-                return tester
-                    .setup.user.state('states:passport_origin')
-                    .input('1')
-                    .check.interaction({
-                        state: 'states:passport_no',
-                        reply: 'Please enter your Passport number:'
-                    })
-                    .run();
-            });
-        });
-
-        describe("after the user enters their passport number", function() {
-            it("should thank them and exit", function() {
-                return tester
-                    .setup.user.state('states:passport_no')
-                    .input('12345')
-                    .check.interaction({
-                        state: 'states:end_success',
-                        reply: ('Thank you for subscribing to MomConnect. ' +
-                            'You will now start receiving free messages ' +
-                            'about MomConnect. Remember to visit your ' + 
-                            'nearest clinic!')
-                    })
-                    .check.reply.ends_session()
-                    .run();
-            });
-        });
-
-        describe("if the user selects None (id type)", function() {
-            it("should ask for their birth year", function() {
-                return tester
-                    .setup.user.state('states:id_type')
-                    .input('3')
-                    .check.interaction({
-                        state: 'states:birth_year',
-                        reply: ('Since you don\'t have an ID or passport, ' +
-                            'please enter the year that you were born (eg ' +
-                            '1981)')
-                    })
-                    .run();
-            });
-        });
-
-        describe("after the user enters their birth year", function() {
-            it("should ask for their birth month", function() {
-                return tester
-                    .setup.user.state('states:birth_year')
-                    .input('1981')
-                    .check.interaction({
-                        state: 'states:birth_month',
-                        reply: ['Please enter the month that you were born.',
-                            '1. Jan',
-                            '2. Feb',
-                            '3. March',
-                            '4. April',
-                            '5. May',
-                            '6. June',
-                            '7. July',
-                            '8. August',
-                            '9. Sept',
-                            '10. Oct',
-                            '11. Nov',
-                            '12. Dec'
-                        ].join('\n')
-                    })
-                    .run();
-            });
-        });
-
-        describe("after the user enters their birth month", function() {
-            it("should ask for their birth day", function() {
-                return tester
-                    .setup.user.state('states:birth_month')
-                    .input('1')
-                    .check.interaction({
-                        state: 'states:birth_day',
-                        reply: ('Please enter the day that you were born ' +
-                            '(eg 14).')
-                    })
-                    .run();
-            });
-        });
-
-        describe("after the user enters their birth day", function() {
-            it("should thank them and exit", function() {
-                return tester
-                    .setup.user.state('states:birth_day')
-                    .input('14')
-                    .check.interaction({
-                        state: 'states:end_success',
-                        reply: ('Thank you for subscribing to MomConnect. ' +
-                            'You will now start receiving free messages ' +
-                            'about MomConnect. Remember to visit your ' + 
-                            'nearest clinic!')
-                    })
-                    .check.reply.ends_session()
-                    .run();
-            });
-        });
+        
 
     });
 });

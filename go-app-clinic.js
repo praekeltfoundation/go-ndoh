0fbc8d2764646cb707411f2a27713a9cddaaa002
gsvr <gsvanrooyen@gmail.com>
2014-05-13 16:31:38 +0200

    revise counting unique users across conversations - clinic

--- go-app-clinic.js
+++ go-app-clinic.js
@@ -141,8 +141,6 @@ go.app = function() {
         self.init = function() {
             self.metric_prefix = self.im.config.name;
 
-            self.get_and_fire_unique_users();
-
             self.im.on('session:new', function() {
                 self.user.extra.ussd_sessions = go.utils.incr_user_extra(
                     self.user.extra.ussd_sessions, 1);
@@ -159,7 +157,11 @@ go.app = function() {
             });
 
             self.im.user.on('user:new', function(e) {
-                self.im.metrics.fire.inc((self.metric_prefix + ".sum.unique_users"), 1);
+                return Q.all([
+                    self.im.metrics.fire.inc((self.metric_prefix + ".sum.unique_users"), 1),
+                    self.im.metrics.fire.inc(("sum.unique_users"))
+                        // probably double counts users if they are in different conversations
+                ]);
             });
 
             return self.im.contacts
@@ -199,15 +201,6 @@ go.app = function() {
             return $("Please dial back in to {{ USSD_number }} to complete the pregnancy registration.")
                 .context({
                     USSD_number: self.im.config.channel
-                });
-        };
-
-        // unique users for the account (across conversations)
-        self.get_and_fire_unique_users = function() {
-            return self.im
-                .api_request('messagestore.count_inbound_uniques',{})
-                .then(function(result) {
-                    return self.im.metrics.fire.last('sum.unique_users', result.count);
                 });
         };
 

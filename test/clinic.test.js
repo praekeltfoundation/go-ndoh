f39da833fc4dfc0739bb858df11227440309f703
gsvr <gsvanrooyen@gmail.com>
2014-05-28 12:40:39 +0200

    metrics for jembi sends

--- test/clinic.test.js
+++ test/clinic.test.js
@@ -688,6 +688,8 @@ describe("app", function() {
                             assert.deepEqual(metrics['test.clinic.avg.sessions_to_register'].values, [5]);
                             assert.deepEqual(metrics['test.clinic.states:language.no_incomplete'].values, [1, 0]);
                             assert.equal(metrics['test.clinic.states:end_success.no_incomplete'], undefined);
+                            assert.deepEqual(metrics['test.clinic.sum.doc_to_jembi_success'].values, [1]);
+                            assert.deepEqual(metrics['test.clinic.sum.json_to_jembi_success'].values, [1]);
                         })
                         .check.reply.ends_session()
                         .run();
@@ -735,11 +737,52 @@ describe("app", function() {
                             assert.deepEqual(metrics['test.clinic.avg.sessions_to_register'].values, [5]);
                             assert.deepEqual(metrics['test.clinic.percent_incomplete_registrations'].values, [25]);
                             assert.deepEqual(metrics['test.clinic.percent_complete_registrations'].values, [75]);
+                            assert.deepEqual(metrics['test.clinic.sum.doc_to_jembi_success'].values, [1]);
+                            assert.deepEqual(metrics['test.clinic.sum.json_to_jembi_success'].values, [1]);
                         })
                         .check.reply.ends_session()
                         .run();
                 });
             });
+            
+            describe("if jembi sends fail", function() {
+                it.skip("should fire fail metrics", function() {
+                    return tester
+                        .setup.user.addr('+27821234567')
+                        .setup(function(api) {
+                            api.contacts.add( {
+                                msisdn: '+27821234567',
+                                extra : {
+                                    clinic_code: '12345',
+                                    suspect_pregnancy: 'yes',
+                                    id_type: 'sa_id',
+                                    sa_id: '5101025009086',
+                                    birth_year: '1951',
+                                    birth_month: '01',
+                                    birth_day: '02',
+                                    dob: '1951-01-02',
+                                    ussd_sessions: '5'
+                                }
+                            });
+                        })
+                        .setup.user.state('states:language')
+                        .input('1')
+                        .check.interaction({
+                            state: 'states:end_success',
+                            reply: ('Thank you. The pregnant woman will now ' +
+                                'receive weekly messages about her pregnancy ' +
+                                'from the Department of Health.')
+                        })
+                        .check(function(api) {
+                            var metrics = api.metrics.stores.test_metric_store;
+                            assert.deepEqual(metrics['test.clinic.sum.doc_to_jembi_fail'].values, [1]);
+                            assert.deepEqual(metrics['test.clinic.sum.json_to_jembi_fail'].values, [1]);
+                        })
+                        .check.reply.ends_session()
+                        .run();
+                });
+            });
+
         });
 
         describe("when a session is terminated", function() {

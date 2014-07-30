9ac2fcbaed63e675b9b3db5e969914e859241a6b
gsvr <gsvanrooyen@gmail.com>
2014-07-30 10:49:35 +0200

    bring faq browser into public line

--- src/personal.js
+++ src/personal.js
@@ -5,8 +5,10 @@ go.app = function() {
     var App = vumigo.App;
     var Choice = vumigo.states.Choice;
     var ChoiceState = vumigo.states.ChoiceState;
+    var PaginatedChoiceState = vumigo.states.PaginatedChoiceState;
     var EndState = vumigo.states.EndState;
     var FreeText = vumigo.states.FreeText;
+    var BookletState = vumigo.states.BookletState;
 
     var GoNDOH = App.extend(function(self) {
         App.call(self, 'states_start');
@@ -538,6 +540,124 @@ go.app = function() {
               next: 'states_start'
             });
         });
+
+
+
+
+        // FAQ Browser
+        // Select topic
+        self.states.add('states_faq_topics', function(name) {
+            return go.utils.get_snappy_topics(self.im, self.im.config.snappy.default_faq)
+                .then(function(response) {
+                    if (typeof response.data.error  !== 'undefined') {
+                        // TODO Throw proper error
+                        return error;
+                    } else {
+                        return response.data.map(function(d) {
+                            return new Choice(d.id, d.topic);
+                        });
+                    }
+                })
+                .then(function(choices) {
+                    return new PaginatedChoiceState(name, {
+                        question: $('We have gathered the most important information in the areas below. Please select one:'),
+                        choices: choices,
+                        options_per_page: 8,
+                        next: 'states_faq_questions'
+                    });
+                });
+        });
+
+        // Show questions in selected topic
+        self.states.add('states_faq_questions', function(name, opts) {
+            return go.utils.get_snappy_topic_content(self.im, 
+                        self.im.config.snappy.default_faq, self.im.user.answers.states_faq_topics)
+                .then(function(response) {
+                    if (typeof response.data.error  !== 'undefined') {
+                        // TODO Throw proper error
+                        return error;
+                    } else {
+                        var choices = response.data.map(function(d) {
+                            return new Choice(d.id, d.question);
+                        });
+
+                        return new PaginatedChoiceState(name, {
+                            question: $('Please choose a question:'),
+                            choices: choices,
+                            // TODO calculate options_per_page once content length is known
+                            options_per_page: 2,
+                            next: function() {
+                                return {
+                                    name: 'states_faq_answers',
+                                    creator_opts: {
+                                        response: response
+                                    }
+                                };
+                            }
+                        });
+                    }
+                });
+        });
+
+        // Show answer to selected question
+        self.states.add('states_faq_answers', function(name, opts) {
+            var id = self.im.user.answers.states_faq_questions;
+            var index = _.findIndex(opts.response.data, { 'id': id });
+            var footer_text = [
+                    "1. Prev",
+                    "2. Next",
+                    "0. Send to me by SMS"
+                ].join("\n");
+            var num_chars = 160 - footer_text.length;
+            // TODO update footer_text length calc for translations
+            var answer = opts.response.data[index].answer.trim();
+            var sms_content = answer;
+            var answer_split = [];
+
+            while (answer.length > 0 && answer.length > num_chars) {
+                answer_max_str = answer.substr(0,num_chars);
+                space_index = answer_max_str.lastIndexOf(' ');
+                answer_sub = answer.substr(0, space_index);
+                answer_split.push(answer_sub);
+                answer = answer.slice(space_index+1);
+            }
+            answer_split.push(answer);
+
+            return new BookletState(name, {
+                pages: answer_split.length,
+                page_text: function(n) {return answer_split[n];},
+                buttons: {"1": -1, "2": +1, "0": "exit"},
+                footer_text:$(footer_text),
+                next: function() {
+                    return {
+                        name: 'states_faq_end',
+                        creator_opts: {
+                            sms_content: sms_content
+                        }
+                    };
+                }
+            });
+        });
+
+        // FAQ End
+        self.states.add('states_faq_end', function(name, opts) {
+            return new EndState(name, {
+                text: $('Thank you. Your SMS will be delivered shortly.'),
+
+                next: 'states_start',
+
+                events: {
+                    'state:enter': function() {
+                        return self.im.outbound.send_to_user({
+                            endpoint: 'sms',
+                            content: opts.sms_content
+                        });
+                    }
+                }
+            });
+        });
+
+
 
     });
 

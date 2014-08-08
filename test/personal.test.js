4ce3e9f4e9e95d41d7b34bd18ebe17f7b88ca029
gsvr <gsvanrooyen@gmail.com>
2014-08-08 13:14:51 +0200

    update text

--- test/personal.test.js
+++ test/personal.test.js
@@ -328,9 +328,9 @@ describe("app", function() {
                         .check.interaction({
                             state: 'states_language',
                             reply: [
-                                'Welcome to MomConnect. Please choose language:',
-                                '1. Eng',
-                                '2. Afrik',
+                                'Welcome to the Department of Health\'s MomConnect. Choose your language:',
+                                '1. English',
+                                '2. Afrikaans',
                                 '3. Zulu',
                                 '4. Xhosa',
                                 '5. Sotho',
@@ -340,8 +340,6 @@ describe("app", function() {
                         .run();
                 });
             });
-
-
         });
 
         describe("when the user starts a session (no prior timeout)", function() {

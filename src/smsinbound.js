0dcc8fba927b00819bc11da5d41bfb898dfa59a4
gsvr <george@westerncapelabs.com>
2014-11-04 19:56:09 +0200

    save contact seq_start against contact

--- src/smsinbound.js
+++ src/smsinbound.js
@@ -105,16 +105,17 @@ go.app = function() {
 
                 events: {
                     'state:enter': function() {
-                        go.utils.subscription_type_and_rate(self.contact, self.im);
+                        opts = go.utils.subscription_type_and_rate(self.contact, self.im);
                         self.contact.extra.subscription_type = opts.sub_type.toString();
                         self.contact.extra.subscription_rate = opts.sub_rate.toString();
+                        self.contact.extra.subscription_seq_start = opts.sub_seq_start.toString();
 
                         return go.utils
                             .subscription_unsubscribe_all(self.contact, self.im)
                             .then(function() {
                                 return Q.all([
-                                    go.utils.subscription_send_doc(self.contact, self.im, self.metric_prefix, opts),
-                                    self.im.contacts.save(self.contact)
+                                    self.im.contacts.save(self.contact),
+                                    go.utils.subscription_send_doc(self.contact, self.im, self.metric_prefix, opts)
                                 ]);
                             });
                     }

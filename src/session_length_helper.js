c1ef828b1aad21241d5db6d97a69c73c36c8b0cd
Simon de Haan <simon@praekeltfoundation.org>
2014-11-10 17:39:35 +0200

    work on session length helper

---
+++ src/session_length_helper.js
@@ -0,0 +1,109 @@
+var vumigo = require('vumigo_v02');
+var events = vumigo.events;
+var Eventable = events.Eventable;
+//var Q = require('q');
+
+var SessionLengthHelper = Eventable.extend(function(self, im, params) {
+  /**class:SessionLengthHelper
+
+  A helper for common session length calculation tasks.
+
+  :param InteractionMachine im:
+    The interaction machine that the metrics should be run on.
+  */
+  self.im = im;
+
+  self.user = im.user;
+
+  self.now = params.clock || function () { return new Date(); };
+
+  self.metrics_prefix = params.metrics_prefix || 'session_length_helper';
+
+  self.mark = {};
+
+  self.mark.session_start = function () {
+    self.user.metadata.session_length_helper = {};
+    self.user.metadata.session_length_helper.start = Number(self.now());
+    return self;
+  };
+
+  self.mark.session_close = function () {
+    if(!self.user.metadata.session_length_helper) {
+      self.user.metadata.session_length_helper = {};
+    }
+    self.user.metadata.session_length_helper.stop = Number(self.now());
+    return self;
+  };
+
+  self.duration = function() {
+    data = self.user.metadata.session_length_helper;
+    if(data && data.stop && data.start) {
+      return data.stop - data.start;
+    }
+  };
+
+  self.get_today_as_string = function() {
+    var today_iso = this.now().toISOString();
+    return today_iso.split('T')[0];
+  };
+
+  self.ensure_today = function (name) {
+    var sentinel_key_name = [self.metrics_prefix, name, 'sentinel'].join('.');
+    return self.im
+      .api_request('kv.get', {
+        key: sentinel_key_name
+      })
+      .then(function (result) {
+        if(result.value != self.get_today_as_string()) {
+          return self.reset_for_today(name);
+        }
+      });
+  };
+
+  self.reset_for_today = function (name) {
+    var sentinel_key_name = [self.metrics_prefix, name, 'sentinel'].join('.');
+    var key_name = [self.metrics_prefix, name].join('.');
+    return self.im
+      .api_request('kv.set', {
+        key: key_name,
+        value: 0
+      })
+      .then(function (result) {
+        return self.im.api_request('kv.set', {
+          key: sentinel_key_name,
+          value: self.get_today_as_string()
+        });
+      });
+  };
+
+  self.store = function(name) {
+    return self.im
+      .api_request('kv.incr', {
+        key: [self.metrics_prefix, name].join('.'),
+        amount: self.duration()
+      })
+      .then(function (result){
+        return result.value;
+      });
+  };
+
+  self.fire_metrics = function (name) {
+    return '!!!';
+  };
+
+  self.increment_and_fire = function (fn_or_str) {
+    var name = vumigo.utils.maybe_call(fn_or_str, self);
+    return self
+      .ensure_today(name)
+      .then(function (result) {
+        return self.store(name);
+      })
+      .then(function (result) {
+        return self.fire_metrics(name);
+      });
+  };
+
+});
+
+
+this.SessionLengthHelper = SessionLengthHelper;
---
+++ test/session_length_helper.test.js
@@ -0,0 +1,127 @@
+var vumigo = require('vumigo_v02');
+var App = vumigo.App;
+var AppTester = vumigo.AppTester;
+var EndState = vumigo.states.EndState;
+var FreeText = vumigo.states.FreeText;
+var slh = require('../src/session_length_helper');
+var SessionLengthHelper = slh.SessionLengthHelper;
+var Q = require('q');
+var assert = require('assert');
+
+
+describe('SessionLengthHelper', function() {
+
+  var app;
+  var tester;
+  var sessionH;
+  var default_start_time = new Date(2014, 11, 10);
+
+  beforeEach(function() {
+      app = new App('states:test');
+
+      tester = new AppTester(app);
+      // stub out the clock
+      sessionH = new SessionLengthHelper(app.im, {
+        clock: function () {
+          return default_start_time;
+        }
+      });
+
+      app.im.on('session:new', function (e) {
+        sessionH.mark.session_start();
+      });
+
+      app.im.on('session:close', function (e) {
+        sessionH.mark.session_close();
+      });
+
+      app.exit = function () {
+        return sessionH.increment_and_fire(function () {
+          return 'vodacom';
+        });
+      };
+
+      app.states.add('states:test', function(name) {
+        return new FreeText(name, {
+            question: 'This is the first state.',
+            next: 'states:test2'
+        });
+      });
+
+      app.states.add('states:test2', function(name) {
+        return Q(function () {
+          return sessionH.mark.session_close();
+        }).then(function () {
+          return new EndState(name, {
+              text: 'This is the end state.',
+              next: 'states:test'
+          });
+        });
+      });
+
+      tester
+        .setup.config.app({
+            name: 'sessionlengthhelper-tester'
+        });
+  });
+
+  describe('When a new session starts', function () {
+
+      it('should flag the start', function () {
+        return tester
+          .start()
+          .check(function(api, im , app) {
+            var slh = im.user.metadata.session_length_helper;
+            assert.equal(slh.start, Number(default_start_time));
+          })
+          .run();
+      });
+
+      var timehop_tester = function (tester, delta) {
+        return tester
+          .setup.user({
+            state: 'states:test',
+            metadata: {
+              session_length_helper: {
+                start: Number(default_start_time) + delta
+              }
+            }
+          });
+      };
+
+      it('should flag the end', function () {
+        return timehop_tester(tester, -1000)
+          .input('bar')
+          .check(function(api, im , app) {
+            var slh = im.user.metadata.session_length_helper;
+            assert.equal(slh.stop, Number(default_start_time));
+          })
+          .run();
+      });
+
+      it('should calculate the duration', function () {
+        return timehop_tester(tester, -1000)
+          .input('bar')
+          .check(function(api, im , app) {
+            var slh = im.user.metadata.session_length_helper;
+            assert.equal(slh.stop, Number(new Date(2014, 11, 10)));
+            assert.equal(sessionH.duration(), 1000);  // milliseconds
+          })
+          .run();
+      });
+
+      it('should add the duration to a daily total', function () {
+        return timehop_tester(tester, -1000)
+          .input('bar')
+          .check(function(api, im , app) {
+            var store = api.kv.store;
+            assert.equal(store['session_length_helper.vodacom'], 1000);
+            assert.equal(
+              store['session_length_helper.vodacom.sentinel'],
+              '2014-12-09');
+          })
+          .run();
+      });
+  });
+
+});
